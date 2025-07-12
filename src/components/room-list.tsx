4d31996c6174c373b7e82535edff016fe6180b36
Michelle Cordeiro <mic321@gmail.com>
2025-07-12 14:30:47 -0300

    refactor: add RoomList component in CreateRoom page

---
+++ src/components/room-list.tsx
@@ -0,0 +1,77 @@
+import { useQuery } from '@tanstack/react-query'
+import { ArrowBigRight } from 'lucide-react'
+import { Link } from 'react-router-dom'
+import { dayjs } from '@/lib/dayjs'
+import { Badge } from './ui/badge'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+
+type GetRoomsAPIResponse = Array<{
+  id: string
+  name: string
+  questionsCount: number
+  createdAt: string
+}>
+
+export function RoomList() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms')
+      const result: GetRoomsAPIResponse = await response.json()
+
+      return result
+    },
+  })
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Salas recentes</CardTitle>
+        <CardDescription>
+          Acesso rápido para as salas criadas recentemente
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="flex flex-col gap-3">
+        {isLoading && (
+          <p className="text-muted-foreground text-sm">Carregando salas...</p>
+        )}
+
+        {data?.map((room) => {
+          return (
+            <Link
+              className="flex items-center justify-between rounded-lg border p-3 hover:bg-accent/50"
+              key={room.id}
+              to={`/rooms/${room.id}`}
+            >
+              <div className="flex flex-1 flex-col gap-1">
+                <h3 className="font-medium">{room.name}</h3>
+
+                <div className="flex items-center gap-2">
+                  <Badge className="text-xs" variant="secondary">
+                    {dayjs(room.createdAt).toNow()}
+                  </Badge>
+
+                  <Badge className="text-xs" variant="secondary">
+                    {room.questionsCount} pergunta(s)
+                  </Badge>
+                </div>
+              </div>
+
+              <span className="flex items-center gap-1 text-sm">
+                Entrar
+                <ArrowBigRight className="size-3" />
+              </span>
+            </Link>
+          )
+        })}
+      </CardContent>
+    </Card>
+  )
+}
